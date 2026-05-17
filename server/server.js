bfb15d16bea1c4ad3715a8cdfbb6fc73e4649e5c
Nikhil-037 <nikhil.2428cseai1717@kiet.edu>
2026-05-18 00:23:58 +0530

    Fix Express 5 compatibility issue with catch-all route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use('/api', apiRoutes);
 // Serve frontend in production
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
